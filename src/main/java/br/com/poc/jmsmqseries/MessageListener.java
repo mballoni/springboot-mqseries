1ae71ea50c8801ab69de0a0968d4c7fda26221df
mballoni <marceloballoni@gmail.com>
2018-08-21 22:41:17 -0300

    Persist message into database

--- src/main/java/br/com/poc/jmsmqseries/MessageListener.java
+++ src/main/java/br/com/poc/jmsmqseries/MessageListener.java
@@ -1,5 +1,7 @@
 package br.com.poc.jmsmqseries;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.stereotype.Component;
@@ -10,10 +12,15 @@ import javax.jms.Session;
 @Component
 public class MessageListener {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @JmsListener(destination = "${queue.name}", containerFactory = "myFactory", concurrency = "5-10", id = "ListenerDEV1")
     public void receiveMessage(Email content, MessageHeaders headers, Session session) throws JMSException {
         System.out.println(session.getAcknowledgeMode());
         System.out.println("trace-id: " + headers.get("traceId"));
         System.out.println("Received <" + content + ">");
+
+        jdbcTemplate.update("INSERT INTO emails (destionation, message) VALUE (?, ?)", content.getDestination(), content.getMessage());
     }
 }
